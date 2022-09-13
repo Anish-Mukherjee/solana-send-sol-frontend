81e5d6db75f980430891e256fffae5ee61472b21
Anish <cybertronian217@gmail.com>
2022-09-14 00:56:50 +0530

    Added wallet connect functionality ✅

---
+++ components/WalletContextProvider.tsx
@@ -0,0 +1,25 @@
+import { FC, ReactNode } from "react";
+import * as Web3 from "@solana/web3.js";
+import * as WalletAdapterWallets from "@solana/wallet-adapter-wallets";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+require("@solana/wallet-adapter-react-ui/styles.css");
+
+const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
+  const endpoint = Web3.clusterApiUrl("devnet");
+  const phantom = new WalletAdapterWallets.PhantomWalletAdapter();
+  const solflare = new WalletAdapterWallets.SolflareWalletAdapter();
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={[phantom, solflare]}>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+export default WalletContextProvider;
